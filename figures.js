0bcb41e0afd6e2eeeb583540a207ce137cff15b2
mterczynski <mterczynski271@gmail.com>
2019-05-12 17:22:36 +0200

    added figures

---
+++ figures.js
@@ -0,0 +1,55 @@
+const figures = [
+  {
+    className: 'figure-pyramid',
+    shape: [
+      [0, 1, 0],
+      [1, 1, 1],
+    ]
+  },
+  {
+    className: 'figure-line',
+    shape: [
+      [1],
+      [1],
+      [1],
+      [1],
+    ]
+  },
+  {
+    className: 'figure-z',
+    shape: [
+      [1, 1, 0],
+      [0, 1, 1],
+    ]
+  },
+  {
+    className: 'figure-s',
+    shape: [
+      [0, 1, 1],
+      [1, 1, 0],
+    ]
+  },
+  {
+    className: 'figure-square',
+    shape: [
+      [1, 1],
+      [1, 1],
+    ]
+  },
+  {
+    className: 'figure-j',
+    shape: [
+      [0, 1],
+      [0, 1],
+      [1, 1],
+    ]
+  },
+  {
+    className: 'figure-l',
+    shape: [
+      [1, 0],
+      [1, 0],
+      [1, 1],
+    ]
+  },
+]
