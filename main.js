e4ef871f81823f96df94c72c7b50e5991e621e0f
mterczynski <mterczynski271@gmail.com>
2019-05-12 23:19:24 +0200

    added drawing logic

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const board = document.getElementById("board");
+const boardHtmlElement = document.getElementById("board");
 const scoreHtmlElement = document.getElementById("score");
 const bestScoreHtmlElement = document.getElementById("best-score");
 
@@ -6,7 +6,7 @@ const boardWidth = 10;
 const boardHeight = 15;
 
 let score = 0;
-let gameLoopInterval;
+let gameLoopIntervalId;
 let boardRows = getNewBoardRows();
 let currentFigure = getRandomFigure();
 
@@ -27,16 +27,71 @@ function resetScores() {
 }
 
 function getRandomFigure() {
-  return { ...figures[Math.floor(Math.random() * figures.length)] };
+  let figure = { ...figures[Math.floor(Math.random() * figures.length)] };
+
+  return {
+    shape: [...figure.shape],
+    className: figure.className,
+    y: 0,
+    x: Math.floor(boardWidth / 2 - figure.shape[0].length / 2),
+  };
+}
+
+function getFigureBlockPosition(figure) {
+  // todo
+}
+
+function initKeyEventListener() {
+  document.addEventListener('keypress', ({ key }) => {
+    if (key === 'ArrowLeft' || 'a' || 'A') {
+      translateCurrentShapeIfPossible({ x: -1, y: 0 });
+    } else if (key === 'ArrowRight' || 'd' || 'D') {
+      translateCurrentShapeIfPossible({ x: 1, y: 0 });
+    } else if (key === 'ArrowDown' || 's' || 'S') {
+      slamCurrentFigure();
+    } else if (key === 'ArrowUp' || 'w' || 'W') {
+      rotateCurrentFigureIfPossible();
+    }
+  });
+}
+
+function getHtmlTile(position) {
+  return document.querySelector(`.row:nth-child(${position.y + 1}) .tile:nth-child(${position.x + 1})`);
+}
+
+function translateCurrentShapeIfPossible(vector) {
+  try {
+    // todo
+    let newFigure = {
+      ...currentFigure,
+      x: currentFigure.x + vector.x,
+      y: currentFigure.y + vector.y,
+    }
+
+    let canTranslate = newFigure.shape.filter(block => block === 1)
+      .every(block => true) // todo
+
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function rotateCurrentFigureIfPossible() {
+  // todo
+}
+
+function slamCurrentFigure() {
+  // todo
 }
 
 function resetBoard() {
   boardRows = getNewBoardRows();
-  board.innerHTML = "";
+  boardHtmlElement.innerHTML = "";
   for (let i = 0; i < boardHeight; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
-    board.appendChild(row);
+    boardHtmlElement.appendChild(row);
     for (let j = 0; j < boardWidth; j++) {
       const tile = document.createElement("div");
       tile.classList.add("tile");
@@ -46,11 +101,31 @@ function resetBoard() {
 }
 
 function tick() {
+  if (!translateCurrentShapeIfPossible({ x: 0, y: 1 })) {
+    // todo - lock figure, check for full row, check for loss, spawn new figure if not lost
+  }
+}
 
+function clearBoard() {
+  [...document.querySelectorAll('.tile')].forEach(tile =>
+    tile.className = tile.className
+      .split(' ')
+      .filter(className => !className.startsWith('figure'))
+      .join(' ')
+  )
+}
+
+function drawBlocks() {
+  boardRows.forEach((row, rowIndex) => {
+    row.forEach((tile, tileIndex) => {
+      getHtmlTile({ x: tileIndex, y: rowIndex }).classList.add(tile.className);
+    });
+  });
 }
 
 function render() {
-
+  clearBoard();
+  drawBlocks();
 };
 
 function gameLoop() {
@@ -58,6 +133,8 @@ function gameLoop() {
   render();
 }
 
-gameLoopInterval = setInterval(gameLoop, 500);
-
-restartGame();
+function init() {
+  gameLoopIntervalId = setInterval(gameLoop, 500);
+  initKeyEventListener();
+  restartGame();
+}
