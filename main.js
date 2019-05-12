463ef04268da72ae8452229d9d17367d6a67a111
mterczynski <mterczynski271@gmail.com>
2019-05-12 19:03:13 +0200

    fixed indentation

--- main.js
+++ main.js
@@ -2,14 +2,22 @@ const board = document.getElementById("board");
 const scoreHtmlElement = document.getElementById("score");
 const bestScoreHtmlElement = document.getElementById("best-score");
 
-const boardWith = 10;
+const boardWidth = 10;
 const boardHeight = 15;
 
 let score = 0;
+let gameLoopInterval;
+let boardRows = getNewBoardRows();
+let currentFigure = getRandomFigure();
+
+function getNewBoardRows() {
+  return Array(boardHeight).fill(Array(boardWidth));
+}
 
 function restartGame() {
   resetBoard();
   resetScores();
+  currentFigure = getRandomFigure();
 }
 
 function resetScores() {
@@ -18,18 +26,38 @@ function resetScores() {
   bestScoreHtmlElement.innerHTML = "0";
 }
 
+function getRandomFigure() {
+  return { ...figures[Math.floor(Math.random() * figures.length)] };
+}
+
 function resetBoard() {
+  boardRows = getNewBoardRows();
   board.innerHTML = "";
   for (let i = 0; i < boardHeight; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     board.appendChild(row);
-    for (let j = 0; j < boardWith; j++) {
+    for (let j = 0; j < boardWidth; j++) {
       const tile = document.createElement("div");
       tile.classList.add("tile");
       row.appendChild(tile);
     }
   }
 }
+
+function tick() {
+
+}
+
+function render() {
+
+};
+
+function gameLoop() {
+  tick();
+  render();
+}
+
+gameLoopInterval = setInterval(gameLoop, 500);
 
 restartGame();
