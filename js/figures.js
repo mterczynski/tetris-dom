5412de551c6272fda467e1db830334d6e07413b2
Michał Terczyński <mterczynski271@gmail.com>
2019-06-02 18:48:08 +0200

    extended shapes with rotable property and center of rotation (marked as 2)

--- js/figures.js
+++ js/figures.js
@@ -3,53 +3,60 @@ export const figures = Object.freeze([
     className: 'figure-pyramid',
     shape: [
       [0, 1, 0],
-      [1, 1, 1],
+      [1, 2, 1],
     ],
+    rotable: true,
   },
   {
     className: 'figure-line',
     shape: [
       [1],
       [1],
+      [2],
       [1],
-      [1],
-    ]
+    ],
+    rotable: true,
   },
   {
     className: 'figure-z',
     shape: [
       [1, 1, 0],
-      [0, 1, 1],
-    ]
+      [0, 2, 1],
+    ],
+    rotable: true,
   },
   {
     className: 'figure-s',
     shape: [
-      [0, 1, 1],
+      [0, 2, 1],
       [1, 1, 0],
-    ]
+    ],
+    rotable: true,
   },
   {
     className: 'figure-square',
     shape: [
       [1, 1],
       [1, 1],
-    ]
+    ],
+    rotable: false,
   },
   {
     className: 'figure-j',
     shape: [
       [0, 1],
       [0, 1],
-      [1, 1],
-    ]
+      [1, 2],
+    ],
+    rotable: true,
   },
   {
     className: 'figure-l',
     shape: [
       [1, 0],
       [1, 0],
-      [1, 1],
-    ]
+      [2, 1],
+    ],
+    rotable: true,
   },
 ].map(figure => ({ ...figure, y: -figure.shape.length })));
